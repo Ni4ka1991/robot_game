b624e8f0eacb8722dcd2df8bbaf1234cb197787b
Ni4ka <astahova_nica@mail.ru>
2021-03-18 11:23:53 +0200

    Add file robot_game.py

---
+++ robot_game.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env Python3
+from os import system
+
+#a - move left
+#d - move right
+
+lenght = 20
+robotX = 5
+bombX  = 8
+
+
+while True:
+ system( "clear" )
+
+ if ( robotX == bombX ):
+  print(
+
+
