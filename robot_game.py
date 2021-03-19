d01a6df07d1b2c06c549bdd4ed01e7a9fe6c6c3c
Ni4ka <astahova_nica@mail.ru>
2021-03-19 12:24:47 +0200

    Add new file robot_game.py

--- robot_game.py
+++ robot_game.py
@@ -3,7 +3,7 @@ from os import system
 
 #a - move left
 #d - move right
-
+ 
 lenght = 20
 robotX = 5
 bombX  = 8
@@ -13,6 +13,45 @@ while True:
  system( "clear" )
 
  if ( robotX == bombX ):
-  print(
+  print( "❌❌❌ GAME OVER :-( ❌❌❌" )
+  break
 
 
+# ########## DRAWING THE MAP ############
+
+ x = 1
+ print( "\n" )
+
+
+ while( x <= lenght ):
+  
+  if( x == robotX ):
+    print( "🤠", end = "" )
+ 
+  elif( x == bombX ):
+    print( "💣", end = "")
+
+  else:
+    print( "-", end = "" )
+  x += 1
+
+ print( "\n" )
+
+# #####################################
+
+# ############# CONTROLS ##############
+
+ direction = input( "dir (a/d/x) > " )
+
+ if( direction == "a" ):
+  robotX -= 1
+
+ if( direction == "d" ):
+  robotX += 1 
+
+ if( direction == "x" ):
+  system( "clear" )
+  print( "Thank you for playing!" )
+  break
+
+# ###################################
